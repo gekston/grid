652e773fae5d17d14da2fa083fa57328430f36d7
Nebeschenko Igor <nebeschenkoichor@gmail.com>
2017-03-14 09:15:12 +0200

    add chrome support

--- app/style.js
+++ app/style.js
@@ -1,27 +1,27 @@
-'use strict';
-document.addEventListener('DOMContentLoaded', function(){
+window.addEventListener('load', function(){
+
+let getVariable = function(styles, propertyName) {
+  return String(styles.getPropertyValue(propertyName)).trim();
+};
+let setDocumentVariable = function(propertyName, value) {
+  document.documentElement.style.setProperty(propertyName, value);
+};
 
   let colRange = document.querySelector('.colWidth');
   colRange.addEventListener('input', function(){
     let colWidth = colRange.value + '%';
-    document.documentElement.style.setProperty('--base-col-width', colWidth);
+    setDocumentVariable('--base-col-width', colWidth);
   })
-  /*
-  colorInput.addEventListener('input', function() {
-    document.documentElement.style.setProperty('--box-color', '#' + colorInput.value);
-    if (colorInput.value.length <= 0) {
-      document.documentElement.style.setProperty('--box-color', '#444444');
-    }
-  })*/
+
   let colorInput = document.querySelector('.boxColor');
   let rangeColor = document.querySelectorAll('.boxColor input[type=range]');
   colorInput.addEventListener('input', function(event) {
     if (event.target.classList === 'boxColor__red') {
-      document.documentElement.style.setProperty('--box-color-red', event.target.value);
+      setDocumentVariable('--box-color-red', event.target.value);
     } else if (event.target.classList === 'boxColor__green') {
-      document.documentElement.style.setProperty('--box-color-green', event.target.value);
+      setDocumentVariable('--box-color-green', event.target.value);
     } else {
-      document.documentElement.style.setProperty('--box-color-blue', event.target.value);
+      setDocumentVariable('--box-color-blue', event.target.value);
     }
   })
 })
